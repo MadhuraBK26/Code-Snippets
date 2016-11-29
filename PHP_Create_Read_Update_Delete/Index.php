72f83677981e4a314c3547a77657168ac5d40a36
Madhura B.K <madhura.bk@compassitesinc.com>
2016-11-29 14:45:56 +0530

    Renaming the file and some css added

---
+++ PHP_Create_Read_Update_Delete/Index.php
@@ -0,0 +1,60 @@
+<html>
+</head>
+<body>
+<h3 align="center" style="color:maroon;">Ticket Calculation for Vehicle Parking</h3>
+<style>
+h3 {
+            text-shadow: -6px 2px 2px #999;
+            font-family: "Corben";
+   }
+ body {
+        background-color:#F5DEB3;
+      } 
+ 
+</style>
+<table>
+  <thead>
+  <table width='90%' border=0>
+     <tr>
+      <td bgcolor="mistyrose"><b>Name</td><b>
+      <td bgcolor="mistyrose"><b>Car number</th><b>
+      <td bgcolor="mistyrose"><b>Car model</th>
+      <td bgcolor="mistyrose"><b>Fare per day</th>
+      <td bgcolor="mistyrose"><b>No of days</th>
+      <td bgcolor="mistyrose"><b>No of cars</th>
+      <td bgcolor="mistyrose"><b>Total amount</th>
+      <td bgcolor="mistyrose"><b>Action</th>
+      <th></th>
+      </tr>
+    </thead>
+  <tbody>
+
+  <?php
+    include 'database.php';
+    $pdo = Database::connect();
+    $sql = 'SELECT * FROM VehicleParking ORDER BY id DESC';
+    foreach ($pdo->query($sql) as $row) {
+    echo '<tr>';
+    echo '<td>'. $row['name'] . '</td>';
+    echo '<td>'. $row['carnumber'] . '</td>';
+    echo '<td>'. $row['carmodel'] . '</td>';
+    echo '<td>'. $row['fareperday'] . '</td>';
+    echo '<td>'. $row['noofdays'] . '</td>';
+    echo '<td>'. $row['noofcars'] . '</td>';
+    echo '<td>'. $row['totalamount'] . '</td>';
+    echo '<td width=250>';
+    echo '<a class="button" href="View_record.php?id='.$row['id'].'">Read</a>';
+    echo ' ';
+    echo '<a  href="update.php?id='.$row['id'].'">Update</a>';
+    echo ' ';
+    echo '<a class="button" href="delete.php?id='.$row['id'].'">Delete</a>';
+    echo '</td>';
+    echo '</tr>';
+    }
+     Database::disconnect();
+  ?>
+</tbody>
+</table>
+<br>
+ <a href="create_record.php">Click for Creating a new record</a><br>
+</html>
