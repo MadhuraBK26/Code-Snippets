ceeccbfe2ee3cd9f70dc2d753fd8ad2a3803fd7e
Madhura B.K <madhura.bk@compassitesinc.com>
2016-11-29 13:02:11 +0530

    Added CSS

--- PHP_Create_Read_Update_Delete/delete.php
+++ PHP_Create_Read_Update_Delete/delete.php
@@ -18,20 +18,41 @@
         $q = $pdo->prepare($sql);
         $q->execute(array($id));
         Database::disconnect();
-        header("Location: Viewpage.php");
+        header("Location: Index.php");
          
     }
 ?>
  
 <html>
 <body>
-<h3>Delete a Customer</h3>
+<h3 style="color:maroon">Delete a Customer</h3>
+<style>
+ h3 {
+            text-shadow: -6px 2px 2px #999;
+            font-family: "Corben";
+    }
+    .button {
+    background-color: #E9967A;
+    border: none;
+    color: white;
+    padding: 10px 10px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 15px;
+    margin: 2px 2px;
+    cursor: pointer;
+}
+.button2 {background-color: #008CBA;}
+ body {
+        background-color:#F5DEB3;} 
+
+</style>
     <form  action="delete.php" method="post">
     <input type="hidden" name="id" value="<?php echo $id;?>"/>
     <p class="alert alert-error">Are you sure to delete ?</p>
     <div class="form-actions">
-    <button type="submit">Yes</button>
-    <a  href="Viewpage.php">No</a>
- </form>
+    <button class="button" type="submit">Yes</button>
+     <a class="button button2" href="Index.php">No</a>
 </div>
 </html>
