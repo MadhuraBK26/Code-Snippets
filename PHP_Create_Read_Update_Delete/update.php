4bcf2860fc2401ab25823d45ce95613e345dc640
Madhura B.K <madhura.bk@compassitesinc.com>
2016-12-02 18:03:30 +0530

    Update file using function and common validation file

--- PHP_Create_Read_Update_Delete/update.php
+++ PHP_Create_Read_Update_Delete/update.php
@@ -1,6 +1,7 @@
 <?php
-    require 'database.php';
- 
+require 'common_validation.php';
+
+
     if ( !empty($_GET['id'])) {
         $id = $_REQUEST['id'];
     }
@@ -8,101 +9,59 @@
     if (is_null($id)) {
         header("Location: Index.php");
     }
+  //print_r($_POST);exit;
+   if ($_POST) {
+        
+        $response = buildVehicleParking($_POST);
+        //print_r($response,$_POST);
 
-
-    if ( !empty($_POST)) {
         // keep track validation errors
-        $nameError = null;
-        $carNumberError = null;
-        $carModelError = null;
-        $farePerDayError = null;
-        $noofDaysError = null;
-        $noofCarsError = null;
-         
-        // keep track post values
-        $name        = $_POST['name'];
-        $carNumber   = $_POST['carNumber'];
-        $carModel    = $_POST['carModel'];
-        $farePerDay  = $_POST['farePerDay'];
-        $noofDays    = $_POST['noofDays'];
-        $noofCars    = $_POST['noofCars'];
-        $totalAmount = $_POST['totalAmount'];
-        $totalAmount = $noofDays * $farePerDay * $noofCars;
-    
-    
-    
-    // validate input
-       $valid = true;
-       if (empty($name) || is_numeric($name)) {
-          $nameError = 'Please enter Name in proper format';
-          $valid = false;
-        }
         
-    
-        if (empty($carNumber)) {
-          $carNumberError = 'Please enter Car Number';
-          $valid  = false;
-        }
-        
-    
-        if (empty($carModel)) {
-          $carModelError = 'Please enter Car Model';
-          $valid  = false;
-        }
-        
-    
-        if (empty($farePerDay) || ($farePerDay <= 0) || is_integer($farePerDay)) {
-          $farePerDayError = 'Please enter Fare per day in proper format';
-          $valid = false;
-        }
-        
-    
-        if (empty($noofDays) || ($noofDays <= 0) || is_integer($noofDays)) {
-          $noofDaysError = 'Please enter Number of days in proper format';
-          $valid  = false;
-        }
-        
-    
-        if (empty($noofCars) || ($noofCars <= 0) || is_integer($noofCars)) {
-          $noofCarsError = 'Please enter number of cars in proper format';
-          $valid  = false;
-        }
-        
-        if ($valid) {
+        if ($response['status'])  {
             $pdo = Database::connect();
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $_POST['totalamount'] =  $_POST['noofdays'] *  $_POST['fareperday'] *  $_POST['noofcars'];
             $sql = "UPDATE VehicleParking  set name = ?, carnumber = ?, carmodel = ?, fareperday = ?, noofdays = ?,noofcars = ?,totalamount = ? WHERE id = ?";
             $q = $pdo->prepare($sql);
-            $q->execute(array($name,$carNumber,$carModel,$farePerDay,$noofDays,$noofCars,$totalAmount,$id));
+            $q->execute(array($_POST['name'],$_POST['carnumber'],$_POST['carmodel'],$_POST['fareperday'],$_POST['noofdays'],$_POST['noofcars'],$_POST['totalamount'],$id));
             Database::disconnect();
             header("Location: Index.php");
         }
+    
 
-    } else {
+    } //else {
 
+            //$response = null;
            $pdo = Database::connect();
            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
            $sql = "SELECT * FROM VehicleParking where id = ?";
            $q = $pdo->prepare($sql);
            $q->execute(array($id));
            $data = $q->fetch(PDO::FETCH_ASSOC);
-           $name = $data['name'];
-           $carNumber = $data['carnumber'];
-           $carModel = $data['carmodel'];
-           $farePerDay = $data['fareperday'];
-           $noofDays = $data['noofdays'];
-           $noofCars = $data['noofcars'];
-           $totalAmount = $data['totalamount'];
+           //print_r($data);exit;
+           $_POST['name'] = $data['name'];
+           $_POST['carnumber'] = $data['carnumber'];
+           $_POST['carmodel'] = $data['carmodel'];
+           $_POST['fareperday'] = $data['fareperday'];
+           $_POST['noofdays'] = $data['noofdays'];
+           $_POST['noofcars'] = $data['noofcars'];
+//           $_POST['totalamount'] = $data['totalamount'];
 
         Database::disconnect();
-     }
+    // }
+
+       // echo"<pre>"; print_r($response);exit;
 ?>
+
+
+
 
 
 <html>
 <body>
 <h3 style="color:maroon">Update a Customer</h3>
-<style>
+ <style>
  h3 {
             text-shadow: -6px 2px 2px #999;
             font-family: "Corben";
@@ -120,83 +79,88 @@
     cursor: pointer;
 }
 .button2 {background-color: #008CBA;}
+ </style>
+ </head>
+ <style>
  body {
         background-color:#F5DEB3;} 
-
 </style>
 <form  action="update.php?id=<?php echo $id?>" method="post">
-    <div class="control-group <?php echo !empty($nameError)?'error':'';?>">
-    <table>
-    <tr>
-    <td><i><label>Name</label></td>
-    <td> <input name="name" type="text"  placeholder="Name" value="<?php echo !empty($name)?$name:'';?>">
-    <?php if (!empty($nameError)): ?>
-    <?php echo $nameError;?></span>
-    <?php endif; ?><td>
-    </tr>
-     <tr> 
+ <table>
+     <tr>
+     <td><i> <label>Name</label></td>
+     <td><input name="name" type="text"  placeholder="Name" value="<?php echo !empty ($_POST['name']) ? ($_POST['name']) : '';?>">
+     <?php if (isset($response['messageList']['name'])):?>
+     <?php echo $response['messageList']['name'];?>
+     <?php endif;?>
+     </tr>
+     <tr>
 
-    <div class="control-group <?php echo !empty($carNumberError)?'error':'';?>">
-    <td> <i><label>Car number</label></td>
-    <td> <input name="carNumber" type="text" placeholder="Car Number" value="<?php echo !empty($carNumber)?$carNumber:'';?>">
-    <?php if (!empty($carNumberError)): ?>
-    <?php echo $carNumberError;?></span>
-    <?php endif;?>
-    </td>
-    </tr>
-    <tr>
-               
-  <div class="control-group <?php echo !empty($carmodelError)?'error':'';?>">
-  <td><i><label>Car model</label></td>
-  <td>  <select name ="carModel"  style="max-width:90%" placeholder="Car model" value="<?php echo !empty($carModel)?$carModel:'';?>"  >
-  <option disabled selected value></option>
-  <option value="Maruti">Maruti</option>
-  <option value="Ford">Ford</option>
-  <option value="Volvo">Volvo</option>
-  <option value="Suzuki">Suzuki</option>
-  </select>
-  <?php if (!empty($carModelError)): ?>
-  <?php echo $carModelError;?>
-  <?php endif;?>
-  </td>
-  </tr>
+     <td><i> <label >Car Number</label></td>
+     <td><input name="carnumber" type="text" placeholder="Car number" value="<?php echo !empty ($_POST['carnumber'])? ($_POST['carnumber']): '';?>">
+     <?php if (isset($response['messageList']['carnumber'])):?>
+     <?php echo $response['messageList']['carnumber'];?>
+     <?php endif;?>
+     </td>
+     </tr>
+     <tr>
+
+  
+     <td><i><label>Car Model</label></td>
+     <td>  <select name ="carmodel"  style="max-width:90%" placeholder="Car model" value="<?php echo !empty( $_POST['carmodel']) ?  ($_POST['carmodel']) : '';?>"  >
+     <option disabled selected value>Select</option>
+     <option value="Maruti">Maruti</option>
+     <option value="Ford">Ford</option>
+     <option value="Volvo">Volvo</option>
+     <option value="Suzuki">Suzuki</option>
+     </select>
+     <?php if (isset($response['messageList']['carmodel'])):?>
+     <?php echo $response['messageList']['carmodel'] ;?>
+     <?php endif;?>
+     </td>
+     </tr>
+                      
+     <tr>
+     <td><i><label>Fare per day</label></td>
+     <td> <input name="fareperday" type="text"  placeholder="Fare per day" value="<?php echo !empty($_POST['fareperday']) ? ($_POST['fareperday']) : '';?>">
+     <?php if (isset($response['messageList']['fareperday'])):?>
+     <?php echo $response['messageList']['fareperday'];?>
+     <?php endif;?>
+     </td>
+     </tr>
+     <tr>
+
+     <td><i><label>No of days</label></td>
+     <td> <input name="noofdays" type="text"  placeholder="No of days" value="<?php echo !empty($_POST['noofdays']) ?  ($_POST['noofdays']) : '';?>">
+     <?php if (isset($response['messageList']['noofdays'])):?>
+     <?php echo $response['messageList']['noofdays']; ?>
+     <?php endif;?>
+     </td>
+     </tr>
+     <tr>
+
+     <td><i> <label>No of cars</label></td>
+     <td>  <input name="noofcars" type="text"  placeholder="No of cars" value="<?php echo !empty($_POST['noofcars']) ?  ($_POST['noofcars']) : '';?>">
+     <?php if (isset($response['messageList']['noofcars'])):?>
+     <?php echo $response['messageList']['noofcars'];;?>
+     <?php endif;?>
+     </td>
+     </tr>
+     <tr>
+     </table>
+    <div class="form-actions">
+    <input class="button" type="submit" />
+    <a class="button button2" href="Index.php">Back</a>
+    </div>
+     </form>
     
-  <tr>                  
-  <div class="control-group <?php echo !empty($farePerDayError)?'error':'';?>">
-  <td><i><label>Fare per day</label></td>
-  <td> <input name="farePerDay" type="text"  placeholder="Car model" value="<?php echo !empty($farePerDay)?$farePerDay:'';?>">
-  <?php if (!empty($farePerDayError)): ?>
-  <?php echo $farePerDayError;?></span>
-  <?php endif;?>
-  </td>
-  </tr>
-                       
-  <tr>
-  <div class="control-group <?php echo !empty($noofDaysError)?'error':'';?>">
-  <td><i>  <label>No of days</label></td>
-  <td><input name="noofDays" type="text"  placeholder="No of days" value="<?php echo !empty($noofDays)?$noofDays:'';?>">
-  <?php if (!empty($noofDaysError)): ?>
-  <?php echo $noofDaysError;?></span>
-  <?php endif;?>
-  </td>
-  </tr>
-   
-  <tr>                    
-  <div class="control-group <?php echo !empty($noofCarsError)?'error':'';?>">
-  <td><label>No of cars</label></td>
-  <td><input name="noofCars" type="text"  placeholder="No of cars" value="<?php echo !empty($noofCars)?$noofCars:'';?>">
-  <?php if (!empty($noofCarsError)): ?>
-  <?php echo $noofCarsError;?></span>
-  <?php endif;?>
-  </td>
-  </tr>
-  </table>
-                       
-  <div class="form-actions">
-  <button class="button" type="submit">Update</button>
-  <a class="button button2" href="Index.php">Back</a>
-</div>
-</form>
-</div>
 </body>
 </html>
+
+
+
+
+
+
+
+
