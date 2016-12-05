c193ea25bf08a963331f22ffcb6fd969da31ca5a
Madhura B.K <madhura.bk@compassitesinc.com>
2016-12-05 11:50:14 +0530

    Some minor changes

--- PHP_Create_Read_Update_Delete/common_validation.php
+++ PHP_Create_Read_Update_Delete/common_validation.php
@@ -1,109 +1,52 @@
 <?php
-//echo 'ste';
-//exit;
+
 require 'database.php';
-//error_reporting(0);
-//require 'create_record.php';
-//buildVehicleParking();
-//$valid;
-//if ($_POST) {
-    $_POST['name'];
-    $_POST['carnumber'];
-    $_POST['carmodel'];
-    $_POST['fareperday'];
-    $_POST['noofdays'];
-    $_POST['noofcars'];
-    
-    
- 
 
-
-function buildVehicleParking($POSTArray)
+    
+ function buildVehicleParking($POSTArray)
 {
 
-  
-  
-    if ( !empty($POSTArray)) {
+  if ( !empty($POSTArray)) {
        $valid = true;
 
-    // keep track validation errors
-
-  /*$nameError=null;
-    $carNumberError=null;
-    $carModelError=null;
-    $farePerDayError=null;
-    $noofDaysError=null;
-    $noofCarsError=null;*/
-    
-   //
-   // echo "4";
-   
-   //global $valid;
-   
-  
-    // validate input
- // $GLOBALS['valid'] = true; 
-    //echo "1";
+    // keep track validation error
+ 
     if (empty($POSTArray['name'])) {
         $errorArray['name'] = 'Please enter Name in proper format';
-        //$nameError = 'Please enter Name in proper format';
-        //echo $nameError;
         $valid = false;
-         
-      }
-      // return  $nameError;
-
+     }
+      
         
-        
-    
-     if (empty($POSTArray['carnumber'])) {
+    if (empty($POSTArray['carnumber'])) {
         $errorArray['carnumber'] = 'Please enter Car Number';
-       // echo  $carNumberError;
         $valid  = false;
       }
         
-        
-    
-     if (empty($POSTArray['carmodel'])) {
+    if (empty($POSTArray['carmodel'])) {
         $errorArray['carmodel'] = 'Please enter Car Model';
-       // echo $carModelError;
         $valid  = false;
       }
         
-        
-    
-     if (empty($POSTArray['fareperday'])) {
+    if (empty($POSTArray['fareperday'])) {
         $errorArray['fareperday'] = 'Please enter Fare per day in proper format';
-       // echo  $farePerDayError;
         $valid = false;
       }
         
-        
-    
-     if (empty($POSTArray['noofdays']))  {
+    if (empty($POSTArray['noofdays']))  {
         $errorArray['noofdays'] = 'Please enter Number of days in proper format';
-        //echo  $noofDaysError;
         $valid  = false;
       }
         
-        
-    
-     if (empty($POSTArray['noofcars'])) {
+    if (empty($POSTArray['noofcars'])) {
         $errorArray['noofcars'] = 'Please enter number of cars in proper format';
-       // echo $noofCarsError;
         $valid  = false;
       }
-    // require 'create_record.php';
-    //}
-
+   
       $response['messageList'] = $errorArray;
       $response['status'] = $valid;
       return $response;
   }
 }
 
-//buildVehicleParking();
- //echo 1;
- 
 
 ?>
