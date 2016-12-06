7405e9dc1d4e6565fc3e1c3e1a8e295b287bc87b
Madhura B.K <madhura.bk@compassitesinc.com>
2016-12-06 14:01:47 +0530

    Final calculation file

--- PHP_Create_Read_Update_Delete/total_calculation.php
+++ PHP_Create_Read_Update_Delete/total_calculation.php
@@ -1,13 +1,9 @@
 <?php
 
-$fareperday =  $_POST['fareperday'];
-$noofdays =  $_POST['noofdays'];
-$noofcars =   $_POST['noofcars'];
-
- function calculateTotal($fareperday,$noofdays,$noofcars)
- {
- 	
-   $total =  $fareperday * $noofdays   *  $noofcars;
-   return  $total;
+function calculateTotal($fareperday,$noofdays,$noofcars)
+{
+ 	$total =  $fareperday * $noofdays   *  $noofcars;
+    return  $total;
 }
- ?>
+ 
+?>
