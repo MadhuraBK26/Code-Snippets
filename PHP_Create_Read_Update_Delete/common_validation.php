1322071d43adc914726b43a5126ac3e1bec7da17
Madhura B.K <madhura.bk@compassitesinc.com>
2016-12-07 11:42:07 +0530

    Modified the code according to PSR

--- PHP_Create_Read_Update_Delete/common_validation.php
+++ PHP_Create_Read_Update_Delete/common_validation.php
@@ -1,54 +1,49 @@
 <?php
 class VehicleParking
 {
+     public  function buildVehicleParking($POSTArray)
+     {
 
-
-
-public  function buildVehicleParking($POSTArray)
-{
-
-  if ( !empty($POSTArray)) {
-       $valid = true;
+         if ( !empty($POSTArray)) {
+            $valid = true;
 
     // keep track validation error
- 
-    if (empty($POSTArray['name'])) {
-        $errorArray['name'] = 'Please enter Name in proper format';
-        $valid = false;
-     }
+         if (empty($POSTArray['name'])) {
+             $errorArray['name'] = 'Please enter Name in proper format';
+             $valid = false;
+         }
       
         
-    if (empty($POSTArray['carnumber'])) {
-        $errorArray['carnumber'] = 'Please enter Car Number';
-        $valid  = false;
-      }
+         if (empty($POSTArray['carnumber'])) {
+             $errorArray['carnumber'] = 'Please enter Car Number';
+             $valid  = false;
+         }
         
-    if (empty($POSTArray['carmodel'])) {
-        $errorArray['carmodel'] = 'Please enter Car Model';
-        $valid  = false;
-      }
+         if (empty($POSTArray['carmodel'])) {
+             $errorArray['carmodel'] = 'Please enter Car Model';
+             $valid  = false;
+         }
         
-    if (empty($POSTArray['fareperday'])) {
-        $errorArray['fareperday'] = 'Please enter Fare per day in proper format';
-        $valid = false;
-      }
+         if (empty($POSTArray['fareperday'])) {
+             $errorArray['fareperday'] = 'Please enter Fare per day in proper format';
+             $valid = false;
+         }
         
-    if (empty($POSTArray['noofdays']))  {
-        $errorArray['noofdays'] = 'Please enter Number of days in proper format';
-        $valid  = false;
-      }
+         if (empty($POSTArray['noofdays']))  {
+             $errorArray['noofdays'] = 'Please enter Number of days in proper format';
+             $valid  = false;
+         }
         
-    if (empty($POSTArray['noofcars'])) {
-        $errorArray['noofcars'] = 'Please enter number of cars in proper format';
-        $valid  = false;
-      }
-   
+         if (empty($POSTArray['noofcars'])) {
+             $errorArray['noofcars'] = 'Please enter number of cars in proper format';
+             $valid  = false;
+         }
+    
       $response['messageList'] = $errorArray;
       $response['status'] = $valid;
       return $response;
+    }
   }
 }
-}
-
 
 ?>
